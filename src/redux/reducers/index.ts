fdb333e3be5ec3f5cdb5b80336337d7a1b2a4d6e
Luqman Mohammad <123764610+luqi2000@users.noreply.github.com>
2023-11-08 22:17:55 +0000

    created files for redux

---
+++ src/redux/reducers/index.ts
@@ -0,0 +1,4 @@
+//here we define our pure function this will be our principal reducer
+//reducer will take the current state of the app when it will called and it takes also action that arriving from a dispatch()
+//It will read the type and with this 2 information will create a new global state.
+//we need to start from a initial State.
---
+++ src/redux/store/index.ts
@@ -0,0 +1,2 @@
+//this file will create our redux store at the running of the application
+//configureStore need structure of the global store/state
