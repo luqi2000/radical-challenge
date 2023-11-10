56fbdc7b9b9a97bed77e7bfdaec09c958b83dafa
Luqman Mohammad <123764610+luqi2000@users.noreply.github.com>
2023-11-10 15:19:55 +0000

    fixed layout of ListBestSellesPage

--- src/components/ListBestSellersPage.tsx
+++ src/components/ListBestSellersPage.tsx
@@ -2,11 +2,14 @@ import { Button, Col, Container, ListGroup, Row } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import Sidebar from "./Sidebar";
 import { FaRegHeart } from "react-icons/fa";
+import { BiBookOpen } from "react-icons/bi";
 import Search from "./Search";
 import { RootState } from "../redux/store";
+import { useState } from "react";
 
 const ListBestSellersPage = () => {
   const books = useSelector((state: RootState) => state.book.books);
+  const [isClicked, setIsClicked] = useState(false);
 
   return (
     <>
@@ -21,15 +24,17 @@ const ListBestSellersPage = () => {
               </div>
               <ListGroup>
                 {books.map((book, index) => (
-                  <ListGroup.Item key={index}>
+                  <ListGroup.Item key={index} className="d-flex justify-content-between flex-column flex-md-row mb-3">
                     <div className="d-flex">
-                      <p>{book.author}</p>
-                      <p> {book.contributor}</p>
-                      <p>{book.price}</p>
-                      <p>{book.rank}</p>
-                      <Button>
-                        {" "}
-                        <FaRegHeart />
+                      <BiBookOpen className="border-BiBookOpen me-2 fs-3" />
+                      <p className="me-1 text-uppercase">{book.author}</p>
+                      <p className="text-secondary">{book.contributor}</p>
+                    </div>
+                    <div className="d-flex">
+                      <p className="me-5">{book.rank}</p>
+                      <p className="me-5">{book.price} GBP</p>
+                      <Button className="border-0 p-0 bg-transparent" onClick={() => setIsClicked(!isClicked)}>
+                        <FaRegHeart className={`border-BiBookOpen fs-3 ${isClicked ? "custom-bg" : ""}`} />
                       </Button>
                     </div>
                   </ListGroup.Item>
