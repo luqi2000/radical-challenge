75522aa6482291fb8c853ad6b86de7b8e82a00d0
Luqman Mohammad <123764610+luqi2000@users.noreply.github.com>
2023-11-11 12:44:26 +0000

    added layout favorites with data

--- src/components/FavouritesPage.tsx
+++ src/components/FavouritesPage.tsx
@@ -1,7 +1,9 @@
-import { Col, Container, ListGroup, Row } from "react-bootstrap";
+import { Button, Col, Container, ListGroup, Row } from "react-bootstrap";
 import Sidebar from "./Sidebar";
 import { useSelector } from "react-redux";
 import { RootState } from "../redux/store";
+import { BiBookOpen } from "react-icons/bi";
+import { FaRegHeart } from "react-icons/fa";
 
 const FavouritesPage = () => {
   const favorites = useSelector((state: RootState) => state.book.favorites);
@@ -13,9 +15,24 @@ const FavouritesPage = () => {
           <Col className="mt-5">
             <ListGroup>
               {favorites.map(book => (
-                <ListGroup.Item key={book.primary_isbn10}>
-                  {/* Display book details as needed */}
-                  {book.author} - {book.contributor}
+                <ListGroup.Item
+                  key={book.primary_isbn10}
+                  className="d-flex justify-content-between flex-column flex-md-row mb-3">
+                  <div className="d-flex">
+                    <BiBookOpen className="border-BiBookOpen me-2 fs-3" />
+
+                    <p className="me-1 text-uppercase">{book.author}</p>
+                    <p className="text-secondary">{book.contributor}</p>
+                  </div>
+                  <div className="d-flex align-items-center">
+                    <p className="me-5">{book.price} GBP</p>
+                    <p className="me-5">{book.rank}</p>
+                    <Button className="border-0 bg-transparent text-secondary">Edit</Button>
+                    <Button className="border-0 bg-transparent text-secondary ">Delete</Button>
+                    <Button className="border-0 bg-transparent">
+                      <FaRegHeart className="border-BiBookOpen fs-3 " />
+                    </Button>
+                  </div>
                 </ListGroup.Item>
               ))}
             </ListGroup>
