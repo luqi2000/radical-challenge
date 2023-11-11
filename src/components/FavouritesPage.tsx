3202c3b98a324f05798807715a1cfca2b3d53822
Luqman Mohammad <123764610+luqi2000@users.noreply.github.com>
2023-11-11 13:07:47 +0000

    adjusted alignment of favourites page

--- src/components/FavouritesPage.tsx
+++ src/components/FavouritesPage.tsx
@@ -12,30 +12,33 @@ const FavouritesPage = () => {
       <Container fluid>
         <Row className="vh-100">
           <Sidebar />
-          <Col className="mt-5">
-            <ListGroup>
-              {favorites.map(book => (
-                <ListGroup.Item
-                  key={book.primary_isbn10}
-                  className="d-flex justify-content-between flex-column flex-md-row mb-3">
-                  <div className="d-flex">
-                    <BiBookOpen className="border-BiBookOpen me-2 fs-3" />
+          <Col className="mt-5 d-flex flex-column align-items-center">
+            <div>
+              <h4 className="mt-5">Favourites</h4>
+              <ListGroup className="mt-5">
+                {favorites.map(book => (
+                  <ListGroup.Item
+                    key={book.primary_isbn10}
+                    className="d-flex justify-content-between flex-column flex-md-row mb-3">
+                    <div className="d-flex">
+                      <BiBookOpen className="border-BiBookOpen me-2 fs-3" />
 
-                    <p className="me-1 text-uppercase">{book.author}</p>
-                    <p className="text-secondary">{book.contributor}</p>
-                  </div>
-                  <div className="d-flex align-items-center">
-                    <p className="me-5">{book.price} GBP</p>
-                    <p className="me-5">{book.rank}</p>
-                    <Button className="border-0 bg-transparent text-secondary">Edit</Button>
-                    <Button className="border-0 bg-transparent text-secondary ">Delete</Button>
-                    <Button className="border-0 bg-transparent">
-                      <FaRegHeart className="border-BiBookOpen fs-3 " />
-                    </Button>
-                  </div>
-                </ListGroup.Item>
-              ))}
-            </ListGroup>
+                      <p className="me-1 text-uppercase">{book.author}</p>
+                      <p className="text-secondary me-4">{book.contributor}</p>
+                    </div>
+                    <div className="d-flex align-items-center">
+                      <p className="me-5">{book.price} GBP</p>
+                      <p className="me-5 d-none d-lg-block">{book.rank}</p>
+                      <Button className="border-0 bg-transparent text-secondary">Edit</Button>
+                      <Button className="border-0 bg-transparent text-secondary">Delete</Button>
+                      <Button className="border-0 bg-transparent">
+                        <FaRegHeart className="border-BiBookOpen fs-3 " />
+                      </Button>
+                    </div>
+                  </ListGroup.Item>
+                ))}
+              </ListGroup>
+            </div>
           </Col>
         </Row>
       </Container>
