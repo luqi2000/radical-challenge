f5cdadcf14e7239a334d9ff3e56ebd412cd71132
Luqman Mohammad <123764610+luqi2000@users.noreply.github.com>
2023-11-12 09:54:05 +0000

    from favourites page now i can remove favourites

--- src/components/FavouritesPage.tsx
+++ src/components/FavouritesPage.tsx
@@ -1,12 +1,18 @@
 import { Button, Col, Container, ListGroup, Row } from "react-bootstrap";
 import Sidebar from "./Sidebar";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../redux/store";
 import { BiBookOpen } from "react-icons/bi";
 import { FaRegHeart } from "react-icons/fa";
+import { removeFromFavorites } from "../redux/actions";
 
 const FavouritesPage = () => {
   const favorites = useSelector((state: RootState) => state.book.favorites);
+  const dispatch = useDispatch();
+
+  const handleHeartClick = (bookId: string) => {
+    dispatch(removeFromFavorites(bookId));
+  };
   return (
     <>
       <Container fluid>
@@ -31,8 +37,8 @@ const FavouritesPage = () => {
                       <p className="me-5 d-none d-lg-block">{book.rank}</p>
                       <Button className="border-0 bg-transparent text-secondary">Edit</Button>
                       <Button className="border-0 bg-transparent text-secondary">Delete</Button>
-                      <Button className="border-0 bg-transparent">
-                        <FaRegHeart className="border-BiBookOpen fs-3 " />
+                      <Button className="border-0 bg-transparent" onClick={() => handleHeartClick(book.primary_isbn10)}>
+                        <FaRegHeart className="border-BiBookOpen fs-3" />
                       </Button>
                     </div>
                   </ListGroup.Item>
