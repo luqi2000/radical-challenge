8442e63c78622a25778a40016e9486a9e793374c
Luqman Mohammad <123764610+luqi2000@users.noreply.github.com>
2023-11-12 10:05:20 +0000

    corrected alignment in the list of the FavouritesPage

--- src/components/FavouritesPage.tsx
+++ src/components/FavouritesPage.tsx
@@ -26,15 +26,15 @@ const FavouritesPage = () => {
                   <ListGroup.Item
                     key={book.primary_isbn10}
                     className="d-flex justify-content-between flex-column flex-md-row mb-3">
-                    <div className="d-flex">
+                    <div className="d-flex align-items-center">
                       <BiBookOpen className="border-BiBookOpen me-2 fs-3" />
 
-                      <p className="me-1 text-uppercase">{book.author}</p>
-                      <p className="text-secondary me-4">{book.contributor}</p>
+                      <div className="me-1 text-uppercase">{book.author}</div>
+                      <div className="text-secondary me-4">{book.contributor}</div>
                     </div>
                     <div className="d-flex align-items-center">
-                      <p className="me-5">{book.price} GBP</p>
-                      <p className="me-5 d-none d-lg-block">{book.rank}</p>
+                      <div className="me-5">{book.price} GBP</div>
+                      <div className="me-5 d-none d-lg-block">{book.rank}</div>
                       <Button className="border-0 bg-transparent text-secondary">Edit</Button>
                       <Button className="border-0 bg-transparent text-secondary">Delete</Button>
                       <Button className="border-0 bg-transparent" onClick={() => handleHeartClick(book.primary_isbn10)}>
